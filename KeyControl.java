bb5e82b954fe306fa8a5f5c88f61b92757287417
Pero <75300217+SomeOtherGod@users.noreply.github.com>
2021-01-05 19:07:23 +0100

    Nohas Klasse gefixed da noha es nicht geschafft hat (Fehler mit abstracten methoden & extends mit Implements ersetst)

--- KeyControl.java
+++ KeyControl.java
@@ -1,25 +1,26 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-class KeyControl extends KeyListener{
+class KeyControl implements KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
     @Override
     public void keyPressed(KeyEvent e) {
         // TODO: implement fucntion calls to move the character
-        switch (e.getKeyCode()){
-            case KeyEvent.VK_LEFT:
-                System.out.println("Pressed left");
-                break;
-            case KeyEvent.VK_RIGHT:
-                System.out.println("Pressed right");
-                break;
-            case KeyEvent.VK_UP:
-                System.out.println("Pressed up");
-                break;
-            case KeyEvent.VK_DOWN:
-                System.out.println("Pressed down");
-                break;
-            default:
-                System.out.println("Pressed key: " + e.getKeyCode());
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_LEFT -> System.out.println("Pressed left");
+            case KeyEvent.VK_RIGHT -> System.out.println("Pressed right");
+            case KeyEvent.VK_UP -> System.out.println("Pressed up");
+            case KeyEvent.VK_DOWN -> System.out.println("Pressed down");
+            default -> System.out.println("Pressed key: " + e.getKeyCode());
         }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
