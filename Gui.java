d076b35f98e7b77d941734c4818e7e03ee1d2bbc
Pero <75300217+SomeOtherGod@users.noreply.github.com>
2021-01-05 17:34:15 +0100

    thread fheler start() > run() umsortieren der elemente

--- Gui.java
+++ Gui.java
@@ -2,28 +2,35 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Gui extends Thread {
-    public synchronized void start() {
-        World w = new World(); // xx/yy position von Pacman
+    World w = new World(); // xx/yy position von Pacman
 
-        JFrame jf = new JFrame("Pacman"); // name des Fensters
+    JFrame jf = new JFrame("Pacman"); // name des Fensters
+
+    JPanel p00 = new JPanel();
+    JPanel p01 = new JPanel();
+    JPanel p02 = new JPanel();
+    JPanel p03 = new JPanel();
+    JPanel p10 = new JPanel();
+    JPanel p11 = new JPanel();
+    JPanel p12 = new JPanel();
+    JPanel p13 = new JPanel();
+    JPanel p20 = new JPanel();
+    JPanel p21 = new JPanel();
+    JPanel p22 = new JPanel();
+    JPanel p23 = new JPanel();
+    JPanel p30 = new JPanel();
+    JPanel p31 = new JPanel();
+    JPanel p32 = new JPanel();
+    JPanel p33 = new JPanel();
+    JPanel[][] jPanels = {{p00,p01,p02,p03},{p10,p11,p12,p13},{p20,p21,p22,p23},{p30,p31,p32,p33}};
+
+    public synchronized void run() {
+
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel p00 = new JPanel();
-        JPanel p01 = new JPanel();
-        JPanel p02 = new JPanel();
-        JPanel p03 = new JPanel();
-        JPanel p10 = new JPanel();
-        JPanel p11 = new JPanel();
-        JPanel p12 = new JPanel();
-        JPanel p13 = new JPanel();
-        JPanel p20 = new JPanel();
-        JPanel p21 = new JPanel();
-        JPanel p22 = new JPanel();
-        JPanel p23 = new JPanel();
-        JPanel p30 = new JPanel();
-        JPanel p31 = new JPanel();
-        JPanel p32 = new JPanel();
-        JPanel p33 = new JPanel();
-        JPanel[][] jPanels = {{p00,p01,p02,p03},{p10,p11,p12,p13},{p20,p21,p22,p23},{p30,p31,p32,p33}};
+        jf.setBounds(0,0, 800, 800);
+        jf.setLocationRelativeTo(null);
+
+
         for (int x = 0; x < 4; x++) {
             for (int y = 0; y < 4; y++) {
                 if (!w.getXyWorld()[x][y]) {
@@ -35,8 +42,10 @@ public class Gui extends Thread {
                 }
             }
         }
+
         jf.setIconImage(new ImageIcon("C:\\tmp/23-237769_pac-man-pixel-art-pac-man.png").getImage()); //img als icon
         jf.setLayout(new GridLayout(4,4));
+
         jf.add(p00);
         jf.add(p01);
         jf.add(p02);
@@ -53,7 +62,7 @@ public class Gui extends Thread {
         jf.add(p31);
         jf.add(p32);
         jf.add(p33);
-        jf.setBounds(0,0,400,400);
+
         jf.setVisible(true);
     }
 }
