93e267cd8c17a792ceedaa90ae15c004d9bcee4e
Pero <75300217+SomeOtherGod@users.noreply.github.com>
2021-01-07 20:28:25 +0100

    basis Auto movement ohne widerholung

--- src/models/Pacman.java
+++ src/models/Pacman.java
@@ -2,37 +2,58 @@ package src.models;
 
 public class Pacman {
 
+    String direction;
+
     World world1;
 
     public Pacman(World w) {
         world1 = w;
     }
 
+    public void move(){
+        switch (direction){
+            case "u":
+                if(world1.y-1 >= 0 && world1.xyWorld[world1.y-1%world1.xyWorld.length][world1.x]){
+                    world1.y--;
+                }
+                break;
+            case "d":
+                if(world1.y+1 < world1.xyWorld.length && world1.xyWorld[world1.y+1%world1.xyWorld.length][world1.x]){
+                    world1.y++;
+                }
+                break;
+            case "l":
+                if(world1.x-1 >= 0 && world1.xyWorld[world1.y][world1.x-1%world1.xyWorld[0].length]){
+                    world1.x--;
+                }
+                break;
+            case "r":
+                if(world1.x+1 < world1.xyWorld[0].length && world1.xyWorld[world1.y][world1.x+1%world1.xyWorld[0].length]){
+                    world1.x++;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
     //UP: Y wird kleiner
     public void moveUp(){
-        if(world1.y-1 >= 0 && world1.xyWorld[world1.y-1%world1.xyWorld.length][world1.x]){
-            world1.y--;
-        }
+        direction = "u";
     }
 
     //DOWN: Y wird größer
     public void moveDown(){
-        if(world1.y+1 < world1.xyWorld.length && world1.xyWorld[world1.y+1%world1.xyWorld.length][world1.x]){
-            world1.y++;
-        }
+        direction = "d";
     }
 
     //LEFT: X wird kleiner
     public void moveLeft(){
-        if(world1.x-1 >= 0 && world1.xyWorld[world1.y][world1.x-1%world1.xyWorld[0].length]){
-            world1.x--;
-        }
+        direction = "l";
     }
 
     //RIGHT: X wird größer
     public void moveRight(){
-        if(world1.x+1 < world1.xyWorld[0].length && world1.xyWorld[world1.y][world1.x+1%world1.xyWorld[0].length]){
-            world1.x++;
-        }
+        direction = "r";
     }
 }
