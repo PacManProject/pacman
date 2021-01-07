3477749aef5580accecfc285c03941c2e880aeb0
Pero <75300217+SomeOtherGod@users.noreply.github.com>
2021-01-07 10:34:07 +0100

    KeyControl outprint mit methoden ersetzt

--- KeyControl.java
+++ KeyControl.java
@@ -2,6 +2,13 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 class KeyControl implements KeyListener {
+
+    Pacman pacman;
+
+    public KeyControl(Pacman p) {
+        pacman = p;
+    }
+
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -11,10 +18,10 @@ class KeyControl implements KeyListener {
     public void keyPressed(KeyEvent e) {
         // TODO: implement fucntion calls to move the character
         switch (e.getKeyCode()) {
-            case KeyEvent.VK_LEFT -> System.out.println("Pressed left");
-            case KeyEvent.VK_RIGHT -> System.out.println("Pressed right");
-            case KeyEvent.VK_UP -> System.out.println("Pressed up");
-            case KeyEvent.VK_DOWN -> System.out.println("Pressed down");
+            case KeyEvent.VK_LEFT -> pacman.moveLeft();
+            case KeyEvent.VK_RIGHT -> pacman.moveRight();
+            case KeyEvent.VK_UP -> pacman.moveUp();
+            case KeyEvent.VK_DOWN -> pacman.moveDown();
             default -> System.out.println("Pressed key: " + e.getKeyCode());
         }
     }
