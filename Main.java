9bc9b454bacd5a97f07b1a69cc49c94a8677f296
SomeOtherGod <75300217+SomeOtherGod@users.noreply.github.com>
2021-01-10 18:56:57 +0100

    neue map

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 
 
 public class Main {
-    static World w = new World("Leandro_macht_die_besten_Karten.json");
+    static World w = new World("map3.json");
     static Pacman p = new Pacman(w);
     static Gui g = new Gui(w, p);
 
