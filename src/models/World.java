a522f893d90ed3fabf7230d8a501ff1d85d12a55
SomeOtherGod <75300217+SomeOtherGod@users.noreply.github.com>
2021-01-11 14:57:06 +0100

    itemXyWorld arrey um zu speichern wo pacman schon war aber ändert auch xyWorld

--- src/models/World.java
+++ src/models/World.java
@@ -18,6 +18,7 @@ public class World {
 
     String name;
     boolean[][] xyWorld;
+    boolean[][] itemXyWorld;
 
     public World(String... mapName) {
 
@@ -38,10 +39,15 @@ public class World {
         this.y = map.pos_y;
         this.name = map.name;
         this.xyWorld = map.data;
+        this.itemXyWorld = xyWorld;
     }
 
     public boolean[][] getXyWorld() {
         return xyWorld;
+    }
+
+    public boolean[][] getItemXyWorld() {
+        return itemXyWorld;
     }
 
     public int getX() {
@@ -51,4 +57,6 @@ public class World {
     public int getY() {
         return y;
     }
+
+    public void up() {}
 }
